e78b712bcf64bc7fd4dcd8ca3ea27ab9aabb2ae5
Zax4r <zaharbedarik@gmail.com>
2024-09-08 21:18:26 +0300

    doxygens

--- PPOIS(lab1)/html/search/functions_0.js
+++ PPOIS(lab1)/html/search/functions_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['getlength_0',['getLength',['../class_vector.html#ae1579f9e2ccb8aa4d39a1135870aed76',1,'Vector']]],
+  ['getlength_0',['getLength',['../class_vector.html#a900f88848b433686d0f47b86b1b7084a',1,'Vector']]],
   ['getpoint_1',['getPoint',['../class_vector.html#a76511613d0ad68ec1d74b071dcd91f06',1,'Vector']]]
 ];
