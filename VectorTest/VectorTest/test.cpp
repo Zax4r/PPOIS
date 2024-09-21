79738e08cf7eb03af6fe0225ac387022bd55bc62
Zax4r <zaharbedarik@gmail.com>
2024-09-21 13:22:45 +0300

    12312321312321321

--- VectorTest/VectorTest/test.cpp
+++ VectorTest/VectorTest/test.cpp
@@ -1,119 +1,100 @@
 #include "pch.h"
 #include "Vector.h"
-TEST(TestCaseName, TestVectorGetLengthPos) {
+TEST(Vector_Test, TestVectorGetLengthPos) {
 	Vector a(0, 0, 3, 4);
   EXPECT_EQ(a.getLength(), 5);
-  EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorGetLengthNeg) {
+TEST(Vector_Test, TestVectorGetLengthNeg) {
 	Vector a(0, 0, -3, -4);
 	EXPECT_EQ(a.getLength(), 5);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorGetLengthDif) {
+TEST(Vector_Test, TestVectorGetLengthDif) {
 	Vector a(0, 0, -3, 4);
 	EXPECT_EQ(a.getLength(), 5);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorGetPoint) {
+TEST(Vector_Test, TestVectorGetPoint) {
 	Vector a(0, 0, 3, 4);
 	Point A(0, 0), B(3, 4);
 	auto Para = std::pair<Point, Point>(A,B);
 	EXPECT_EQ(a.getPoint(),Para);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorSumOperatorWithPos) {
+TEST(Vector_Test, TestVectorSumOperatorWithPos) {
 	Vector a(0, 0, 3, 4),b(0,0,2,8);
 	EXPECT_EQ((a+b).getLength(),13);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorSumOperatorWithNeg) {
+TEST(Vector_Test, TestVectorSumOperatorWithNeg) {
 	Vector a(0, 0, 3, 4), b(0, 0, -3,-4);
 	EXPECT_EQ((a+b).getLength(), 0);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorSubtOperatorWithPos) {
+TEST(Vector_Test, TestVectorSubtOperatorWithPos) {
 	Vector a(5, 2, 3, 4), b(5, 2, 3, 4),nulVec(0, 0, 0, 0);
 	EXPECT_EQ((a-b), nulVec);
 	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorSubtOperatorWithNeg) {
+TEST(Vector_Test, TestVectorSubtOperatorWithNeg) {
 	Vector a(5, 2, 3, 4), b(-5,- 2, -3, -4);
 	EXPECT_EQ((a - b).getLength(),sqrt(32));
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorMultOperWithPosNumb) {
+TEST(Vector_Test, TestVectorMultOperWithPosNumb) {
 	Vector a(0, 0, 3, 4);
 	EXPECT_EQ((a*2).getLength(), 10);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorMultOperWithNegNumb) {
+TEST(Vector_Test, TestVectorMultOperWithNegNumb) {
 	Vector a(0, 0, 3, 4);
 	EXPECT_EQ((a*-2).getLength(), 10);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorDotOperWithPosVect) {
+TEST(Vector_Test, TestVectorDotOperWithPosVect) {
 	Vector a(0, 0, 3, 4),b(1,2,5,10);
 	EXPECT_EQ(a*b,44);
-	EXPECT_TRUE(true);
 }
 
 
-TEST(TestCaseName, TestVectorDotOperWithNegVect) {
+TEST(Vector_Test, TestVectorDotOperWithNegVect) {
 	Vector a(2,1, 3, 4), b(-1, -2, -5, -10);
 	EXPECT_EQ(a * b, -28);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorAngleOperWithOpposVect) {
+TEST(Vector_Test, TestVectorAngleOperWithOpposVect) {
 	Vector a(2, 1, 3, 4), b(-2, -1,- 3,- 4);
 	EXPECT_EQ(a^b,-1);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorAngleOperWithSameVect) {
+TEST(Vector_Test, TestVectorAngleOperWithSameVect) {
 	Vector a(2, 1, 3, 4), b(4, 2, 6, 8);
 	EXPECT_EQ(a ^ b, 1);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorGreThOperatorWithVect) {
+TEST(Vector_Test, TestVectorGreThOperatorWithVect) {
 	Vector a(0, 0, 3, 4), b(0, 0, -3, -4);
 	EXPECT_EQ(a>b,false);
-	EXPECT_TRUE(true);
 }
 
-TEST(TestCaseName, TestVectorCopyConstr) {
+TEST(Vector_Test, TestVectorCopyConstr) {
 	Vector a(0, 0, 3, 4);
 	Vector b(a);
-	EXPECT_EQ(a==b,true);
-	EXPECT_TRUE(true);
+	EXPECT_EQ(a,b);
 }
 
-TEST(TestCaseName, TestVectorAssigmentOper) {
+TEST(Vector_Test, TestVectorAssigmentOper) {
 	Vector a(0, 0, 3, 4);
 	Vector b(0,0,0,0);
 	b = a;
-	EXPECT_EQ(a == b, true);
-	EXPECT_TRUE(true);
+	EXPECT_EQ(a ,b);
 }
 
-TEST(TestCaseName, TestVectorAssigmentOperDel) {
+TEST(Vector_Test, TestVectorEqualOper) {
 	Vector a(0, 0, 3, 4);
-	Vector b(0, 0, 0, 0);
-	b = a;
-	a.~Vector();
-	EXPECT_EQ(b.getLength(), 5);
-	EXPECT_TRUE(true);
+	Vector b(0, 0, 3, 4);
+	EXPECT_EQ(a==b,true);
 	std::cin.get();
 }
